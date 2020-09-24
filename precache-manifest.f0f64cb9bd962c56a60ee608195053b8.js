ec0cc0fe6ea48cd4aad1595c75c99380a8e7ca53
Osamah Anwar <anwar.osamah@gmail.com>
2020-09-23 22:50:58 -0500

    Updates

---
+++ precache-manifest.f0f64cb9bd962c56a60ee608195053b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e0a2dfc47c8bde95751f9b9d93d0da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6301593e422192a512b0",
+    "url": "/static/css/main.6ad4d55f.chunk.css"
+  },
+  {
+    "revision": "4a0959724a57dff36ebf",
+    "url": "/static/js/2.596bc2ff.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.596bc2ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6301593e422192a512b0",
+    "url": "/static/js/main.cbdb6a54.chunk.js"
+  },
+  {
+    "revision": "66db21fdc6eb50a98001",
+    "url": "/static/js/runtime-main.8971bd07.js"
+  },
+  {
+    "revision": "5175131e97889f19b56ebd50fa2a7756",
+    "url": "/static/media/handshake.5175131e.svg"
+  }
+]);
